3faff647b95c7cb5f47c978e565eb056e53d1ded
mhgffqwoer <a.yakshevich2005@gmail.com>
2025-02-26 15:32:56 +0300

    feat: added UserBase
    added UserBase interface, UserBuilder interface, abstract factory

---
+++ internal/User/UserBase.go
@@ -0,0 +1,31 @@
+package user
+
+import "errors"
+
+type UserType int
+
+const (
+	Student UserType = iota
+	Teacher
+)
+
+type UserBase interface {
+	GetName() string
+	GetID() int
+}
+
+type UserBuilder interface {
+	SetName(name string)
+	Build() (UserBase, error)
+}
+
+func New(userType UserType) (UserBuilder, error) {
+	switch userType {
+	case Student:
+		return nil, nil
+	case Teacher:
+		return nil, nil
+	default:
+		return nil, errors.New(`invalid user type`)
+	}
+}
